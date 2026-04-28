f2ae5179c5eb340ba99ae01d83565919a763b2ca
Cunsonglee <leecunsong@gmail.com>
2026-04-28 09:05:05 +0200

    Delete for_anki.py

--- for_anki.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import json
-
-out = []
-with open("./final.json", "r", encoding="utf-8") as f:
-    dic = json.load(f)
-    for word, meaning in dic.items():
-        out.append("\t".join([word, meaning]))
-Path.write_text(Path("./final.txt"), "\n".join(out), encoding="utf-8")
